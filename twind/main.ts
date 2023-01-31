8d7deb96d57e370ba8d9ad8e341a336d57b7c6d2
y3km21 <y3km21@gmail.com>
2023-01-31 22:44:49 +0900

    Fix target in insert method

--- twind/main.ts
+++ twind/main.ts
@@ -20,15 +20,17 @@ export function hydrate(options: Options, state: State) {
   cssom_sheet.insert = (cssText, index) => {
     // https://github.com/tw-in-js/twind/blob/main/packages/core/src/sheets.ts#L61
     try {
-      // Insert
       // Filter cssrule set by virtual.
       // Add only the cssrule that first appears in island.
       if (!virtualCssSheetSet.has(cssText)) {
-        target.insertRule(cssText, target.cssRules.length);
+        cssom_sheet.target.insertRule(
+          cssText,
+          cssom_sheet.target.cssRules.length
+        );
       }
     } catch (error) {
       // Empty rule to keep index valid — not using `*{}` as that would show up in all rules (DX)
-      target.insertRule(":root{}", index);
+      cssom_sheet.target.insertRule(":root{}", index);
 
       // Some thrown errors are because of specific pseudo classes
       // lets filter them to prevent unnecessary warnings
