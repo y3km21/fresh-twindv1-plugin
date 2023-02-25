96d59fbec3669c8f1270c1a1e6606aa7c29dc09d
y3km21 <y3km21@gmail.com>
2023-02-25 21:19:59 +0900

    fix: Bind resume from sheet obtained from getSheet

--- twind/main.ts
+++ twind/main.ts
@@ -1,8 +1,8 @@
 // Original Code
 // https://github.com/denoland/fresh/blob/main/plugins/twind/main.ts
 //
-import { cssom, stringify, Sheet, SheetRule } from "twind";
-import { STYLE_ELEMENT_ID, Options, setup } from "./shared.ts";
+import { cssom, getSheet, setup } from "twind";
+import { STYLE_ELEMENT_ID, Options } from "./shared.ts";
 
 type State = string[];
 
@@ -15,67 +15,12 @@ export function hydrate(options: Options) {
 
   const el = document.getElementById(STYLE_ELEMENT_ID) as HTMLStyleElement;
   const cssom_sheet = cssom(el);
-  cssom_sheet.resume = resume.bind(cssom_sheet);
+
+  // https://github.com/tw-in-js/twind/blob/main/packages/core/src/sheets.ts#L177
+  const sheetWithResumeFeature = getSheet();
+  document.querySelector('[data-twind="claimed"]')?.remove();
+
+  cssom_sheet.resume = sheetWithResumeFeature.resume.bind(cssom_sheet);
 
   setup(options, cssom_sheet);
-}
-
-// https://github.com/tw-in-js/twind/blob/main/packages/core/src/sheets.ts#L206
-function resume(
-  this: Sheet,
-  addClassName: (className: string) => void,
-  insert: (cssText: string, rule: SheetRule) => void
-) {
-  // hydration from SSR sheet
-  const textContent = stringify(this.target);
-  const RE = /\/\*!([\da-z]+),([\da-z]+)(?:,(.+?))?\*\//g;
-
-  // only if this is a hydratable sheet
-  if (RE.test(textContent)) {
-    // RE has global flag — reset index to get the first match as well
-    RE.lastIndex = 0;
-
-    // 1. start with a fresh sheet
-    this.clear();
-
-    // 2. add all existing class attributes to the token/className cache
-    if (typeof document != "undefined") {
-      // deno-lint-ignore no-explicit-any
-      for (const el of document.querySelectorAll("[class]") as any) {
-        addClassName(el.getAttribute("class") as string);
-      }
-    }
-
-    // 3. parse SSR styles
-    let lastMatch: RegExpExecArray | null | undefined;
-
-    while (
-      (function commit(match?: RegExpExecArray | null) {
-        if (lastMatch) {
-          // const tc = textContent.slice(
-          //   lastMatch.index + lastMatch[0].length,
-          //   match?.index
-          // );
-          // console.log(tc);
-
-          insert(
-            // grep the cssText from the previous match end up to this match start
-            textContent.slice(
-              lastMatch.index + lastMatch[0].length,
-              match?.index
-            ),
-            {
-              p: parseInt(lastMatch[1], 36),
-              o: parseInt(lastMatch[2], 36) / 2,
-              n: lastMatch[3],
-            }
-          );
-        }
-
-        return (lastMatch = match);
-      })(RE.exec(textContent))
-    ) {
-      /* no-op */
-    }
-  }
 }
